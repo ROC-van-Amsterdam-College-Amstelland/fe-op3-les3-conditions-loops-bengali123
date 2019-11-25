0840a98288d89b20ad564fcb7303d6d716606a32
bengali <bengalidoumbia000@gmail.com>
2019-11-25 15:38:08 +0100

    opdracht 1

--- conditions_opdrachten/conditions_opdracht_1/js/script.js
+++ conditions_opdrachten/conditions_opdracht_1/js/script.js
@@ -1,1 +1,10 @@
 //javascript code
+function check() {
+    var saldo = document.getElementById("saldo").value;
+
+    if (saldo<25) {
+        document.getElementById("tekst").innerHTML = "uw saldo is te laag"
+    } else {
+        document.getElementById("tekst").innerHTML = "";
+    }
+}
