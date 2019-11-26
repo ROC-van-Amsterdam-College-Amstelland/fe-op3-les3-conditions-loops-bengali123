fa491b4f4b3c7e8fe23ea3aee59cc25c82facd2b
bengali <bengalidoumbia000@gmail.com>
2019-11-26 13:08:50 +0100

    af

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,1 +1,9 @@
-//javascript code
+//javascript codefunction check() {
+    var saldo = document.getElementById("saldo").value;
+
+    if (saldo>500) {
+        document.getElementById("tekst").innerHTML = "Je hebt voldoende saldo om"
+    } else {
+        document.getElementById("tekst").innerHTML = "Sorry, je hebt te weinig saldo!";
+    }
+}
